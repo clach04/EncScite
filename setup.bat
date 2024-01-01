87cee33864e9638800eb3686a79a46ff377e6020
clach04 <clach04@gmail.com>
2023-12-31 16:22:56 -0800

    comments for zip if pip not available

--- setup.bat
+++ setup.bat
@@ -3,6 +3,11 @@ git clone https://github.com/clach04/puren_tonbo.git
 ::git clone https://github.com/clach04/openssl_enc_compat.git
 ::git clone 
 
+:: wget https://codeload.github.com/clach04/puren_tonbo/zip/refs/heads/main
+:: wget https://codeload.github.com/clach04/openssl_enc_compat/zip/refs/heads/main
+:: wget https://codeload.github.com/clach04/chi_io/zip/refs/heads/main
+:: wget https://codeload.github.com/tartley/colorama/zip/refs/heads/master
+
 
 :: curl https://www.scintilla.org/wscite32_541.zip
 curl --output sc1.exe https://www.scintilla.org/Sc32_541.exe
